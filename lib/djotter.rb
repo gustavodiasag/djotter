0eb7328fcaa6a442ffcad48088506406e701bb38
gustavodiasag <gustavodias.aguiar1@gmail.com>
2025-08-12 20:58:24 -0300

    chore: formatting

--- lib/djotter.rb
+++ lib/djotter.rb
@@ -5,12 +5,11 @@ require "djotter/djotter"
 
 module Djotter
   class << self
-
-  # Public: Parses and converts a Djot string into an HTML string.
-  #
-  # content - A {String} of text
-  #
-  # Returns a {String} of converted HTML.
+    # Public: Parses and converts a Djot string into an HTML string.
+    #
+    # content - A {String} of text
+    #
+    # Returns a {String} of converted HTML.
     def to_html(content)
       raise TypeError, "text must be as String; got #{content.class}" unless content.is_a?(String)
       raise TypeError, "text must e UTF-8 encoded; got #{content.encoding}" unless content.encoding.name == "UTF-8"
